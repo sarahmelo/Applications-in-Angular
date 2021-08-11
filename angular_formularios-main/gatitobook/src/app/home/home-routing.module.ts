fe720dd50acfa00bc952495cc639166241f07b70
sarahmelo <sarahcbdg@gmail.com>
2021-08-11 19:19:19 -0300

    Modules

---
+++ angular_formularios-main/gatitobook/src/app/home/home-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+  {path:'', component: HomeComponent,
+    children: [
+      {path: '', component: LoginComponent}
+    ]
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
